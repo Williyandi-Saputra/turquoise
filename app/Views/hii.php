3e09b86e3f5da1408cc78db9eb2f3665f41b33a5
Willyandi Saputra <williyandi_saputra@teknokrat.ac.id>
2022-12-15 11:03:09 +0700

    membuat tampilan hii

---
+++ app/Views/hii.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h1> UDAH LAH PULANG AJA GAA USAH NGODING </h1>
+<h1> NGODING ITU GAA PENTING !! </h1>
+
+<div>  
+
+    <h1> Bakso Ari Lasso </h1>
+
+    <h4> ENTAH DIMANAA </h4>
+    <span> bakso kon bakso kon </span>
+    <br><br><br>
+    <a href="hii"> beli bakso </a>
+
+</div>
+    
+</body>
+</html>
