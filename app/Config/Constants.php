a028ed272d54d390cce89bf0445aa7a55370d375
Suwarman Adi Wardana <suwarman_adi_wardana@teknokrat.ac.id>
2023-01-07 10:24:53 +0700

    update constans

--- app/Config/Constants.php
+++ app/Config/Constants.php
@@ -92,3 +92,6 @@ define('EVENT_PRIORITY_NORMAL', 100);
  * @deprecated Use \CodeIgniter\Events\Events::PRIORITY_HIGH instead.
  */
 define('EVENT_PRIORITY_HIGH', 10);
+
+define('NAMA_FOLDER', 'http://localhost:8080/riter/');
+define('FOLDER_DASHBOARD', 'http://localhost:8080/dd/');
